bade3c2036f8b4a2da7b2b851341f3b621f12488
Alex Busko <alexbusko@icloud.com>
2022-01-13 19:00:56 +0200

    tab & name fixes

--- moonbeam/moonbeam_update.sh
+++ moonbeam/moonbeam_update.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 if [ ! $MOONBEAM_NODENAME ]; then
-read -p "Enter node name: " MOONBEAM_NODENAME
-echo 'export MOONBEAM_NODENAME='${MOONBEAM_NODENAME} >> $HOME/.bash_profile
+	read -p "Enter node name: " MOONBEAM_NODENAME
+	echo 'export MOONBEAM_NODENAME='${MOONBEAM_NODENAME} >> $HOME/.bash_profile
 fi
 
 export UID=${UID}
 export GID=${GID}
-export NODE_NAME="$MOONBEAM_NODENAME"
 
 docker-compose down
 docker-compose pull
