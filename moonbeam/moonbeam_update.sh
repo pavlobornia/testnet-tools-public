d417478598b4085e0e9dfdae664192be27bdae03
AlexBusko <52037504+AlexBusko@users.noreply.github.com>
2022-01-13 20:38:12 +0200

    Feat/moonbeam (#5)
    * add moonbeam docker-compose.yaml
    
    * add docker-compose
    
    * move file
    
    * update commands
    
    * add moonbeam update bash
    
    * remove wrong docker-compose file
    
    * if else bash added
    
    * tab & name fixes
    
    * add read bashprofile

---
+++ moonbeam/moonbeam_update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ ! $MOONBEAM_NODENAME ]; then
+	read -p "Enter node name: " MOONBEAM_NODENAME
+	echo 'export MOONBEAM_NODENAME='${MOONBEAM_NODENAME} >> $HOME/.bash_profile
+	.$HOME/.bash_profile
+fi
+
+export UID=${UID}
+export GID=${GID}
+
+docker-compose down
+docker-compose pull
+docker-compose up -d
