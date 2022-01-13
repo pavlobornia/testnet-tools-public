04d4c523d234a38f35e07aca1e70841d91eaa97c
Pavlo Bornia <pbornia@lohika.com>
2022-01-13 22:29:33 +0200

    fix: compose and script

--- moonbeam/moonbeam_update.sh
+++ moonbeam/moonbeam_update.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+source $HOME/.bash_profile
 
 if [ ! $MOONBEAM_NODENAME ]; then
 	read -p "Enter node name: " MOONBEAM_NODENAME
 	echo 'export MOONBEAM_NODENAME='${MOONBEAM_NODENAME} >> $HOME/.bash_profile
-	. $HOME/.bash_profile
+	source $HOME/.bash_profile
 fi
 
-export UID=${UID}
-export GID=${GID}
+export uid=`id -u`
+export gid=`id -g`
 
 docker-compose down
 docker-compose pull
