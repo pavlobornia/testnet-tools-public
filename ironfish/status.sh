095cb3c3ef747db7d8391f183e93bef23776b985
Alex Busko <maraboosko@MacBook-Pro-Alex.local>
2022-05-30 12:25:53 +0300

    add timer and some changes

--- ironfish/status.sh
+++ ironfish/status.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
+start=$(date +%s)
+
 curl -s https://raw.githubusercontent.com/pavlobornia/testnet-tools-public/master/logo/logo.sh | bash
 
-echo "Перезавантажуємо сервіс IronFish"
+echo "1. Перезавантажуємо сервіс IronFish"
+systemctl restart ironfishd
+echo "Сервіс перезавантажено"
 
-sleep 1
-
-service ironfishd restart
-
-sleep 1
-
+sleep 5
+echo ""
 echo "Перевіряємо статус"
-
 ironfish status
 
+echo ""
 echo "Перевіряємо баланс"
-
 ironfish accounts:balance
+
+end=$(date +%s)
+seconds=$(echo "$end - $start" | bc)
+
+echo $seconds' sec'
+
 
 
 
