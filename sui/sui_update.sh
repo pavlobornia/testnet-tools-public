20bd652cc52c044ca22bbfccdf641dc9b67f3265
Alex Busko <maraboosko@yulia-HP-Pavilion-g6-Notebook-PC.localdomain>
2022-08-24 13:40:21 +0300

    change sui update script

--- sui/sui_update.sh
+++ sui/sui_update.sh
@@ -12,11 +12,12 @@ cd sui
 git remote add upstream https://github.com/MystenLabs/sui
 git fetch upstream
 git checkout -B devnet --track upstream/devnet
-cargo build --release -p sui-node
+cargo build -p sui-node -p sui --release 
 mv ~/sui/target/release/sui-node /usr/local/bin/
+mv ~/sui/target/release/sui /usr/local/bin/
 wget -O /var/sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
 systemctl restart suid
 echo ""
 echo "Нода оновлена. Перевіряємо логи"
 sleep 10
-journalctl -n 100 -f -u suid.service   
+journalctl -n 100 -f -u suid.service    
