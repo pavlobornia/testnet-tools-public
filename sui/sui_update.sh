a76f5745f623b992a7d24e8b973753e2dfa66474
Alex Busko <maraboosko@yulia-HP-Pavilion-g6-Notebook-PC.localdomain>
2022-08-31 11:36:44 +0300

    sui 0.8.0

--- sui/sui_update.sh
+++ sui/sui_update.sh
@@ -2,16 +2,14 @@
 
 curl https://raw.githubusercontent.com/pavlobornia/testnet-tools-public/master/logo/logo.sh | bash
 echo "Залітайте в наше Discord комюніті https://discord.gg/weSuTQ2Dx7"
-echo "Оновлення ноди Sui до версії 0.7.2 встановленої за гайдом NodesGuru."
+echo "Оновлення ноди Sui до версії 0.8.0 встановленої за гайдом NodesGuru."
 systemctl stop suid
 rm -rf /var/sui/db/* /var/sui/genesis.blob $HOME/sui
 source $HOME/.cargo/env
 cd $HOME
 git clone https://github.com/MystenLabs/sui.git
 cd sui
-git remote add upstream https://github.com/MystenLabs/sui
-git fetch upstream
-git checkout -B devnet --track upstream/devnet
+git checkout devnet-0.8.0
 cargo build -p sui-node -p sui --release 
 mv ~/sui/target/release/sui-node /usr/local/bin/
 mv ~/sui/target/release/sui /usr/local/bin/
