5ced00c98aed0cc4914253b74393b0655392c524
Alex Busko <maraboosko@yulia-HP-Pavilion-g6-Notebook-PC.localdomain>
2022-09-07 10:51:24 +0300

    sui 0.9.0 script

--- sui/sui_update.sh
+++ sui/sui_update.sh
@@ -2,14 +2,16 @@
 
 curl https://raw.githubusercontent.com/pavlobornia/testnet-tools-public/master/logo/logo.sh | bash
 echo "Залітайте в наше Discord комюніті https://discord.gg/weSuTQ2Dx7"
-echo "Оновлення ноди Sui до версії 0.8.0 встановленої за гайдом NodesGuru."
+echo "Оновлення ноди Sui до версії 0.9.0 встановленої за гайдом NodesGuru."
 systemctl stop suid
 rm -rf /var/sui/db/* /var/sui/genesis.blob $HOME/sui
 source $HOME/.cargo/env
 cd $HOME
 git clone https://github.com/MystenLabs/sui.git
 cd sui
-git checkout devnet-0.8.0
+git remote add upstream https://github.com/MystenLabs/sui
+git fetch upstream
+git checkout -B devnet --track upstream/devnet
 cargo build -p sui-node -p sui --release 
 mv ~/sui/target/release/sui-node /usr/local/bin/
 mv ~/sui/target/release/sui /usr/local/bin/
@@ -18,4 +20,9 @@ systemctl restart suid
 echo ""
 echo "Нода оновлена. Перевіряємо логи"
 sleep 10
-journalctl -n 100 -f -u suid.service    
+if dpkg --list | grep -q ccze;
+then
+        journalctl -n 100 -f -u suid.service | ccze -A
+else
+        journalctl -n 100 -f -u suid.service
+fi   
