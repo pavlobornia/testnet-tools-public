73d9bf29b2e3f8201947c7ca9e5a53b650a4f325
Alex Busko <maraboosko@yulia-HP-Pavilion-g6-Notebook-PC.localdomain>
2022-10-21 15:21:52 +0300

    sui v0.12.1

--- sui/sui_update.sh
+++ sui/sui_update.sh
@@ -2,7 +2,7 @@
 
 curl https://raw.githubusercontent.com/pavlobornia/testnet-tools-public/master/logo/logo.sh | bash
 echo "Залітайте в наше Discord комюніті https://discord.gg/weSuTQ2Dx7"
-echo "Оновлення ноди Sui до версії 0.12.0 встановленої за гайдом NodesGuru."
+echo "Оновлення ноди Sui до версії 0.12.1 встановленої за гайдом NodesGuru."
 systemctl stop suid.service
 rm -rf /var/sui/db/* /var/sui/genesis.blob $HOME/sui
 source $HOME/.cargo/env
@@ -18,6 +18,7 @@ mv ~/sui/target/release/sui /usr/local/bin/
 wget -O /var/sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
 systemctl restart suid.service
 echo ""
+sui -V
 echo "Нода оновлена. Перевіряємо логи"
 sleep 10
 if dpkg --list | grep -q ccze;
