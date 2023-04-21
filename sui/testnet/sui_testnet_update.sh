e3a415800677cd0ba544ae753908c020ac9c1986
Alex Busko <maraboosko@MacBook-Pro-Alex.local>
2023-04-21 11:54:33 +0300

    sui update

--- sui/testnet/sui_testnet_update.sh
+++ sui/testnet/sui_testnet_update.sh
@@ -2,32 +2,29 @@
 
 curl https://raw.githubusercontent.com/pavlobornia/testnet-tools-public/master/logo/logo.sh | bash
 echo "Залітайте в наше Discord комюніті https://discord.gg/weSuTQ2Dx7"
-read -p "Видалити базу даних? (y/n) " answer
+# read -p "Видалити базу даних? (y/n) " answer
 systemctl stop suid.service
-cd $HOME 
-if [ "$answer" == "y" ]; then
-  rm -rf /var/sui/suidb/* 
-  echo "База даних видалена."
-fi
+# cd $HOME 
+# if [ "$answer" == "y" ]; then
+#   rm -rf /var/sui/suidb/* 
+#   echo "База даних видалена."
+# fi
+
 rm -rf sui 
 git clone https://github.com/MystenLabs/sui.git
 cd sui
-git remote add upstream https://github.com/MystenLabs/sui
 git fetch upstream
-git checkout -B testnet --track upstream/testnet
-git log --oneline -1
+git reset --hard 133fd85bcd79ed048a897be3a3f3bd4a2e9c616a
 cargo build --release --bin sui-node
 sudo mv ~/sui/target/release/sui-node /usr/local/bin/
-sudo mv ~/sui/target/release/sui /usr/local/bin/
 systemctl restart suid.service
-echo ""
-sui-node --version
-echo "Нода оновлена. Перевіряємо логи"
-sleep 10
-if dpkg --list | grep -q ccze;
-then
-        journalctl -fn 10 -u suid.service | ccze -A
-else
-        journalctl -fn 10 -u suid.service
-fi   
-
+# echo ""
+# sui-node --version
+# echo "Нода оновлена. Перевіряємо логи"
+# sleep 10
+# if dpkg --list | grep -q ccze;
+# then
+#         journalctl -fn 10 -u suid.service | ccze -A
+# else
+#         journalctl -fn 10 -u suid.service
+# fi   
