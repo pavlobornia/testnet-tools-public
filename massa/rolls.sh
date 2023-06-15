18077ad97396e45494c141ff5597bbe250477cb9
pavlobornia <36510617+pavlobornia@users.noreply.github.com>
2023-06-15 16:52:04 +0300

    fix roll script 23.x episode (#40)
    Co-authored-by: Pavlo Bornia <pbornia@lohika.com>

--- massa/rolls.sh
+++ massa/rolls.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
-
-rm -f $HOME/massa/massa-client/massa-client
-if [ ! -e $HOME/massa/massa-client/massa-client ]; then
-  wget https://raw.githubusercontent.com/pavlobornia/testnet-tools-public/master/massa/massa-client -O $HOME/massa/massa-client/massa-client
-  chmod +x $HOME/massa/massa-client/massa-client
-fi
-
 cd $HOME/massa/massa-client
-massa_wallet_address=$(./massa-client wallet_info | grep Address | awk '{ print $2 }')
+massa_wallet_address=$(./massa-client --pwd $massa_password wallet_info | grep Address | awk '{ print $2 }')
 while true
 do
-        balance=$(./massa-client wallet_info | grep "Final balance" | awk '{ print $3 }')
+        balance=$(./massa-client --pwd $massa_password wallet_info | grep 'Balance' | awk '{print $3}' | sed 's/=/ /'|sed 's/,/ /'|awk '{print $2}')
         int_balance=${balance%%.*}
         if [ $int_balance -gt "99" ]; then
                 echo "More than 99"
-                resp=$(./massa-client buy_rolls $massa_wallet_address $(($int_balance/100)) 0)
+                resp=$(./massa-client --pwd $massa_password buy_rolls $massa_wallet_address $(($int_balance/100)) 0)
                 echo $resp
         elif [ $int_balance -lt "100" ]; then
                 echo "Less than 100"
